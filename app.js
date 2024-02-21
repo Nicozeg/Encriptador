9f3a99f80a637972a652c9d183746de0222e1d94
Nicozeg <157088513+Nicozeg@users.noreply.github.com>
2024-02-21 13:40:56 -0600

    Crea el app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Challenge Encriptador Alura Latam//
+//variables//
+let palabraUsuario="hola mundo";
+let letras=[
+    "a","ai",
+    "e","enter",
+    "i","imes",
+    "o","ober",
+    "u","ufat"];
+let palabraEncriptada="";
+for (let i=0;i<=palabraUsuario.length;i++){
+    
+    console.log (i);
+
+}
