202d07830127f14d2f0835ace64badd822e93224
Nicozeg <jayson.baide@gmail.com>
2024-03-01 19:07:39 -0600

    actualiza los archivos

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-// Challenge Encriptador Alura Latam//
-//variables//
-let palabraUsuario="hola mundo";
-let letras=[
-    "a","ai",
-    "e","enter",
-    "i","imes",
-    "o","ober",
-    "u","ufat"];
-let palabraEncriptada="";
-for (let i=0;i<=palabraUsuario.length;i++){
-    
-    console.log (palabraUsuario.substring(i-1,i));
 
-}
